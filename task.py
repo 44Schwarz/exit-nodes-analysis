3d6eb9d3d96748446f7841fabf69f258d151841b
44Schwarz <sirmitya@gmail.com>
2019-10-02 11:58:03 +0300

    Refactor code

--- task.py
+++ task.py
@@ -23,26 +23,32 @@ def unpack_archive(file):
         pass
 
 
+def read_file(file):
+    lines = list()
+    try:
+        with open(file) as f:
+            next(f)
+            next(f)  # skip 2 header lines
+            for line in f:
+                title, data = line.strip().split(' ', 1)
+                if title == 'ExitAddress':
+                    lines.append(data.split(' ', 1))  # split into IP address and date
+    except FileNotFoundError:
+        pass
+    return lines
+
+
 def parse_files(list_of_files):
     nodes_list = list()
+    res = dict()
     for file in list_of_files:
-        lines = list()
-        try:
-            with open(file) as f:
-                next(f)
-                next(f)  # skip 2 header lines
-                for line in f:
-                    title, data = line.strip().split(' ', 1)
-                    if title == 'ExitAddress':
-                        lines.append(data.split(' ', 1))  # split into IP address and date
-        except FileNotFoundError:
-            pass
+        lines = read_file(file)
         nodes_list.extend(lines)
 
     titles = ('ExitAddress', 'AddressDate')
     df = pd.DataFrame(nodes_list, columns=titles)
     if df.empty:
-        return dict()
+        return res
 
     gr = df.groupby(['ExitAddress']).size().reset_index(name='Count')
     ip = gr.sort_values(by='Count', ascending=False)['ExitAddress'].iloc[0]  # most frequent IP address
@@ -53,7 +59,6 @@ def parse_files(list_of_files):
 
     unique_ips = df['ExitAddress'].unique().tolist()  # for calculating difference
 
-    res = dict()
     res['ip'] = ip
     res['first_seen'] = date_first
     res['last_seen'] = date_last
@@ -67,17 +72,17 @@ def get_all_files():
     list_of_files = list()
     for file in files:
         list_of_files.append([])
-        for (dirpath, dirnames, filenames) in os.walk(os.path.join(path_to_extract, file.split('.', 1)[0])):
+        dest_dir = file.split('.', 1)[0]  # archive name without extension
+        for (dirpath, dirnames, filenames) in os.walk(os.path.join(path_to_extract, dest_dir)):
             for filename in filenames:
-                list_of_files[-1].append(
-                    os.path.join(os.path.join(path_to_extract, file.split('.', 1)[0], dirpath, filename)))
+                list_of_files[-1].append(os.path.join(path_to_extract, dest_dir, dirpath, filename))  # full file path
 
-    for files_for_month in list_of_files:  # separate results for months
-        parsing = parse_files(files_for_month)
+    for files_for_month in list_of_files:
+        parsing = parse_files(files_for_month)  # calculate separate results for months
         if parsing:
             results.append(parsing)
 
-    if len(results) == len(files):
+    if len(results) == len(files):  # if results exist for both months
         first_ips, second_ips = [results[i].get('unique_ips') for i in (0, 1)]
         results[0]['unique_ips'] = difference(first_ips, second_ips)
         results[1]['unique_ips'] = difference(second_ips, first_ips)
@@ -118,13 +123,14 @@ if __name__ == '__main__':
     except FileExistsError:
         pass
 
-    if os.path.isdir(path_to_extract):
-        for archive in files:
-            try:
-                urllib.request.urlretrieve(os.path.join(url_path, archive), os.path.join(path_to_extract, archive))
-            except URLError:
-                print(f"Error while retrieving a file {archive}")
-            unpack_archive(os.path.join(path_to_extract, archive))
-        get_all_files()
-    else:
+    if not os.path.isdir(path_to_extract):
         print(f"{path_to_extract} is not a directory")
+        exit()
+
+    for archive in files:
+        try:
+            urllib.request.urlretrieve(os.path.join(url_path, archive), os.path.join(path_to_extract, archive))
+        except URLError:  # proceed even if file wasn't downloaded
+            print(f"Error while retrieving a file {archive}")
+        unpack_archive(os.path.join(path_to_extract, archive))
+    get_all_files()
